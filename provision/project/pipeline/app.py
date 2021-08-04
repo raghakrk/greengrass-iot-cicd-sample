ecd5ef817eb72a49826b1bdbc7b22bf08c96e77b
Raghak Radhakrishnan <raghakrk7@gmail.com>
2021-08-03 18:58:26 -0700

    Add cdk deploy (#6)
    * Add cdx stack
    
    * Edit changes
    
    * Add cdk account credentials

--- provision/project/pipeline/app.py
+++ provision/project/pipeline/app.py
@@ -5,7 +5,15 @@ from aws_cdk import core
 
 from lib.greengrass_stack import PipelineStack
 
+CDK_DEFAULT_ACCOUNT='465906353389'
+CDK_DEFAULT_REGION='us-west-2'
+
+env = core.Environment(
+    account=CDK_DEFAULT_ACCOUNT,
+    region=CDK_DEFAULT_REGION,
+)
+
 app = core.App()
-PipelineStack(app, "greengrass_pipeline")
+PipelineStack(app, "greengrasspipeline", env=env)
 
 app.synth()
