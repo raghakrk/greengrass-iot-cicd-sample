5233c503e4616ca29e954717c184646143c32dd4
Raghak Radhakrishnan <raghakrk7@gmail.com>
2021-08-30 18:36:11 +0530

    Update greengrass_stack.py

--- provision/project/pipeline/lib/greengrass_stack.py
+++ provision/project/pipeline/lib/greengrass_stack.py
@@ -1,95 +1,59 @@
+# For consistency with other languages, `cdk` is the preferred import name for
+# the CDK's core module.  The following line also imports it as `core` for use
+# with examples from the CDK Developer's Guide, which are in the process of
+# being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import (
     core,
-    aws_iot as iot,
-    custom_resources as cust_resource,
-    aws_lambda as awslambda,
-    aws_iam as iam,
-    aws_s3 as s3,
-    aws_codebuild as codebuild,
-    aws_codedeploy as code_deploy,
     aws_codecommit as codecommit,
+    aws_codedeploy as code_deploy,
+    aws_iam as iam,
+    aws_codebuild as codebuild,
     aws_codepipeline as codepipeline,
     aws_codepipeline_actions as codepipeline_actions,
-    aws_greengrassv2 as greengrassv2,
-    aws_ssm as ssm
 )
-import uuid
+import os
+import json
+PARAMETER_FILE="./deploy_params.json"
 
-class PipelineStack(core.Stack):
-    """
-    Utility to define PipelineStack
-    """
+if not os.path.exists(PARAMETER_FILE):
+    raise FileNotFoundError("file {} does not exist".format(PARAMETER_FILE))
+
+
+class GreengrassPipelineStack(core.Stack):
+
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        code = codecommit.Repository.from_repository_name(
-            self, "CodeRepo", repository_name="greengrass-iot-cicd-sample"
-        )
+        f = open(PARAMETER_FILE,)
+        deploy_params = json.load(f)
+        f.close()
 
-        # create a server application
+        repository_name = deploy_params['repositoryName']
+        repository_branch = deploy_params['repositoryBranchName']
+        on_premise_instance_tags = deploy_params['onPremiseInstanceTags']
+
+        code = codecommit.Repository.from_repository_name(
+            self, "CodeRepo", repository_name=repository_name
+        )
         serverApplication = code_deploy.ServerApplication(
             self, "GreenGrassDeployment", application_name="GreenGrassDeployment",
         )
-        role_arn="arn:aws:iam::465906353389:role/codedeploy-iot-gg-sample-project-service-role"
+
+        # The code that defines your stack goes here
         code_deploy.ServerDeploymentGroup(
             self,
             "CodeDeploymentGroup",
             application=serverApplication,
             deployment_group_name="GreenGrassDeploymentGroup",
-            role=iam.Role.from_role_arn(
-                    self,
-                    "role",
-                    role_arn=role_arn
-                ),
             deployment_config=code_deploy.ServerDeploymentConfig.ONE_AT_A_TIME,
             on_premise_instance_tags =code_deploy.InstanceTagSet(
-                {"Name": ["JetsonNano"]},
+                on_premise_instance_tags,
             ),
         )
-        
-        # inline_recipe_="""
-        # ---
-        # RecipeFormatVersion: "2020-01-25"
-        # ComponentName: "HelloWorld-example-cpp"
-        # ComponentVersion: "1.0.0"
-        # ComponentType: "aws.greengrass.generic"
-        # ComponentDescription: "My first Greengrass component."
-        # ComponentPublisher: "Me"
-        # ComponentConfiguration:
-        # DefaultConfiguration:
-        #     Message: "world"
-        # Manifests:
-        # - Platform:
-        #     os: "linux"
-        # Name: "Linux"
-        # Lifecycle:
-        #     Run:
-        #     Script: "cd {artifacts:decompressedPath}/deploy_package && ./hello"
-        #     RequiresPrivilege: true
-        # Artifacts:
-        # - Uri: "s3://iot-gg-cicd-sample/deploy_package.zip"
-        #     Algorithm: "SHA-256"
-        #     Unarchive: "ZIP"
-        #     Permission:
-        #     Read: "ALL"
-        #     Execute: "ALL"
-        # Lifecycle: {}           
-        # """
-        # greengrass_component = greengrassv2.CfnComponentVersion(
-        #     self,
-        #     "GreengrassComponent",
-        #     inline_recipe="HelloWorld-example-cpp.yaml"
-        # )
-
-        # for policy_name in greengrass_policy_names:
-        #     greengrass_component.role.add_managed_policy(
-        #         iam.ManagedPolicy.from_aws_managed_policy_name(policy_name)
-        #     )
-        
         cdk_deploy_canary = codebuild.PipelineProject(
             self,
             "Deploy",
-            project_name="iot-gg-cicd-workshop-deploy-canary",
+            project_name="canary",
             build_spec=codebuild.BuildSpec.from_object(dict(
                             version="0.2",
                             phases=dict(
@@ -97,7 +61,7 @@ class PipelineStack(core.Stack):
                                     commands=[
                                         "ls -la",
                                         "cd provision/project/pipeline",
-                                        "python3 deploy.py --target-arn arn:aws:iot:us-west-2:465906353389:thinggroup/canary --deployment-name iot-gg-canary",
+                                        "python3 deploy.py --target-name canary",
                                     ])),
                                 artifacts={
                                 "base-directory": ".",
@@ -131,7 +95,7 @@ class PipelineStack(core.Stack):
                                     commands=[
                                         "ls -la",
                                         "cd provision/project/pipeline",
-                                        "python3 deploy.py --target-arn arn:aws:iot:us-west-2:465906353389:thinggroup/main --deployment-name iot-gg-main",
+                                        "python3 deploy.py --target-name main",
                                     ])),
                                 artifacts={
                                 "base-directory": ".",
@@ -159,16 +123,17 @@ class PipelineStack(core.Stack):
 
         cdk_build_output = codepipeline.Artifact("CdkBuildOutput")
         source_output = codepipeline.Artifact(artifact_name='source')
+        pipeline_name = deploy_params['pipelineName']
         codepipeline.Pipeline(self,
                               "Pipeline",
-                              pipeline_name="iot-gg-cicd-workshop-pipeline-canary",
+                              pipeline_name=pipeline_name,
                               stages=[
                                   codepipeline.StageProps(stage_name="Source",
                                                           actions=[
                                                               codepipeline_actions.CodeCommitSourceAction(
                                                                   action_name="CodeCommit_Source",
                                                                   repository=code,
-                                                                  branch="main",
+                                                                  branch=repository_branch,
                                                     output=source_output)]),
                                   codepipeline.StageProps(stage_name="Build_artifacts",
                                                           actions=[
@@ -185,11 +150,14 @@ class PipelineStack(core.Stack):
                                                           actions=[
                                                               codepipeline_actions.CodeBuildAction(
                                                                   action_name="Build_Package_Deploy_Canary",
-                                                                  project=cdk_deploy_canary)]),
+                                                                  project=cdk_deploy_canary,
+                                                                  input=source_output)]),
                                   codepipeline.StageProps(stage_name="Deploy_in_greengrass_main",
                                                           actions=[
                                                               codepipeline_actions.CodeBuildAction(
                                                                   action_name="Build_Package_Deploy_Main",
-                                                                  project=cdk_deploy_main)]),
+                                                                  project=cdk_deploy_main,
+                                                                  input=source_output)]),
                               ]
                               )
+
